3308ab08915f44b45f892802b329b6fd33ff2b28
FrederykMtl <frederic.simard@gmail.com>
2015-07-25 17:05:24 -0400

    MAJ transfert fichiers

--- GTI785-Lab2-Client/src/com/client/filemanager/File.java
+++ GTI785-Lab2-Client/src/com/client/filemanager/File.java
@@ -9,6 +9,10 @@ public class File {
 		
 	}
 	
+	public File (String name){
+		this.name=name;
+	}
+	
 	public String getName() {
 		return name;
 	}
--- GTI785-Lab2-Client/src/com/client/filemanager/FileManager.java
+++ GTI785-Lab2-Client/src/com/client/filemanager/FileManager.java
@@ -1,21 +1,69 @@
 package com.client.filemanager;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-import com.client.servermanager.ServerObject;
-import com.google.gson.Gson;
+import android.util.Base64;
 
 public class FileManager {
+	//We have a list of server for a client
+	private ArrayList<File> files;
 	
-	private ServerObject currentSelectedServer;
-	private ArrayList<File> currentServerFiles;
+	//Lets make a singleton of this class. per client there is only one server manager
+/*	private static FileManager instance = null;
 	
-	public Gson getFileList(){
-		return null;
-	}
-	
-	public void TransferFile(String urlSource, String urlDestination){
+	public static FileManager getInstance(){
+		if (instance == null)
+			instance = new FileManager();
 		
-	}
+		return instance;
+	}*/
+	
+	public FileManager (){
+		files =  new ArrayList<File>();		
+	}	
 
+	public void setFiles(ArrayList<File> fileList)
+	{
+		files = fileList;
+	}
+	
+	public void setFilesFromUnencodedString(String fileList)
+	{
+		String[] list = fileList.split(",");
+		
+		for(int i=0; i < list.length; i++)
+		{
+			files.add(new File(list[i]));
+		}
+	}
+	
+	public void setFilesFromEncodedString(String fileList) throws UnsupportedEncodingException
+	{
+		setFilesFromUnencodedString(DecodeString(fileList));
+	}
+	
+	public void AddFile(String file)
+	{
+		files.add(new File(file));
+	}
+	
+	public void AddFile(File file)
+	{
+		files.add(file);
+	}
+	
+	public ArrayList<File> getFiles(){
+		return files;
+	}
+	
+	private String DecodeString(String files) throws UnsupportedEncodingException
+	{
+		byte[] octets = Base64.decode(files, Base64.URL_SAFE);
+		return new String(octets, "UTF-8");
+	}
+	
+	public File getFileAt(int position){
+		return files.get(position);
+	}
 }
